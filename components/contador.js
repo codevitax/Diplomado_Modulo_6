8d7f6dc5430b42926e6c0ff3fcc20862bf63fe16
codevitax <122753201+codevitax@users.noreply.github.com>
2023-03-01 18:21:42 -0400

    componentes

--- components/contador.js
+++ components/contador.js
@@ -1,11 +1,12 @@
 Vue.component(
     'contador',
     {
-        template: "<div><p>contador {{numero}}</p><p><button @click='numero += 1'>contador</button></p></div>",
+        template: "<div><p>contador de: {{titulo}} {{numero}}</p><p><button @click='numero += 1'>contador</button></p></div>",
         data: function(){
             return {
                 numero:0
             }
-        }
+        },
+        props: ["titulo"]
     }
 );
